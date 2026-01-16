2cfb3462df2c3d658665c76217df0b869f5612fe
Alex Richert <alexander.richert@noaa.gov>
2026-01-16 10:48:13 -0800

    u

--- spack/package.py
+++ spack/package.py
@@ -2,7 +2,7 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
+from spack_repo.builtin.build_systems.cmake import CMakePackage
 from spack.package import *
 
 
